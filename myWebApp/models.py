0ffc4220dfb4203923cccf46a37f3529b2264c85
a-tromi <148511515+a-tromi@users.noreply.github.com>
2024-04-08 22:05:01 -0400

    Se integra oracle y se crea modelo Usuario

--- myWebApp/models.py
+++ myWebApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Módelo para usuario 
+class Usuario(models.Model):
+    id_usuario = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=60, null=False, default='')
+    email = models.CharField(max_length=50, null=False, default='')
+    password = models.CharField(max_length=18, null=False, default='')
+    fecha_nac = models.DateField()
+
+
+    def __str__(self):
+        return self.id_usuario +' '+ self.email
+    
